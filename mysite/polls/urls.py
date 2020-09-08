ff11fd97746505a93da4a89de8fe963b7821aaba
Phùng Việt <70842031+viettp29@users.noreply.github.com>
2020-09-08 15:53:00 +0700

    abc

---
+++ mysite/polls/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('list/', views.viewlist, name = 'view_list'),
+    path('detail/<int:question_id>',views.detailView, name = 'detail'),
+]
---
+++ mysite/polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Question
+# Create your views here.
+def index(request):
+    myname = "Thế Việt"
+    cuacai = ["500k", "điện thoại A8S clone", "laptop HP Elitebook 8560w"]
+    return render(request, "polls/index.html", {"name":myname, "tien":cuacai})
+
+def viewlist(request):
+    #get_object_or_404(Question, pk=3)
+    list_question = Question.objects.all()
+    context = {"dsquest": list_question}
+    return render(request, "polls/question_list.html", context)
+
+def detailView(request, question_id):
+    q = Question.objects.get(pk=question_id)
+    return render(request, "polls/detail_question.html", {"qs":q})
+
